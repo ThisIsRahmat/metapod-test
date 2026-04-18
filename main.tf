065168b6200455078a0a36d7aa3a6bc0025802e1
rahmat <rahmat@MASTAFF-Unassigned-Device-Q1Y0W3MJ60.local>
2026-04-18 16:07:42 +0100

    improving main tf

--- main.tf
+++ main.tf
@@ -7,14 +7,17 @@ terraform {
   }
 
 
+
+backend "s3" {
+    bucket = "rahmat-acebook"
+    key    = "path/tfstate"
+    region = "eu-west-2"
+  }
+  }
+
+
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-2"
   profile = "rahmat-kluster-user"
 }
-
-backend "s3" {
-    bucket = "rahmat-acebook"
-    key    = "path/tfstate"
-  }
-  }
