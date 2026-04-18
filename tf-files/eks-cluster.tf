ccfba96e47fb05ab1e84f7d39c2cfe88b723458a
Rahmat <thisisrahmat@gmail.com>
2026-04-18 15:50:05 +0100

    Initial commit

---
+++ tf-files/eks-cluster.tf
@@ -0,0 +1,44 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "17.24.0"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.29"
+  subnets         = data.aws_subnets.vpc_subnets.ids
+
+  tags = {
+    Environment = "training"
+  }
+
+  vpc_id = aws_default_vpc.default_vpc.id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      public_ip = true
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+      public_ip = true
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
